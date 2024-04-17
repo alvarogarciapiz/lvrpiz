7e9e71473e2abdff0ab628d4e6d09577f5934cca
alvarogarciapiz <alvarogp99@gmail.com>
2024-04-17 16:18:18 +0200

    Fix import order in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './assets/styles/App.css'
 import Header from './Header.jsx'
-import Content from './Content.jsx'
 import Footer from './Footer.jsx'
+import Content from './Content.jsx'
+
 
 function App() {
   return (
