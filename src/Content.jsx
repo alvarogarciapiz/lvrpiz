65981d5404f899decadbb8c2e3a449ca1085be06
Álvaro García Pizarro <alvarogp99@gmail.com>
2024-09-15 11:15:48 +0200

    Update projects in Content.jsx and add latest project styling

--- src/Content.jsx
+++ src/Content.jsx
@@ -35,12 +35,12 @@ const Content = () => {
             <div className='projectsSpace'>
                 <h2>Projects</h2>
                 <div className='projects'>
-                    <a className='project' href='https://www.commandshow.lvrpiz.com'>The Command</a>
-                    <a className='project' href='https://alvarogarciapiz.github.io/AWS-Certify-Hub/'>AWS Certify Hub</a>
+                    <a className='project latest' href='https://gawg.lvrpiz.com'>GAWG</a>
                     <a className='project' href='https://github.com/alvarogarciapiz/IntelliTweet'>IntelliTweet</a>
-                    <a className='project' href='https://github.com/alvarogarciapiz/GRIPweb'>GRIP Clothing Store</a>
-                    <a className='project' href='https://alvarogarciapiz.github.io/GeniusKeys/'>GeniusKeys</a>
+                    <a className='project' href='https://alvarogarciapiz.github.io/AWS-Certify-Hub/'>AWS Certify Hub</a>
                     <a className='project' href='https://github.com/alvarogarciapiz/MoodWise'>MoodWise</a>
+                    <a className='project' href='https://commandshow.lvrpiz.com'>The Command</a>
+                    <a className='project' href='https://alvarogarciapiz.github.io/GeniusKeys/'>GeniusKeys</a>
                 </div>
             </div>
 
